5928b0c7fc1655e72984607b8834f100f3791e50
Czeglédi Dávid <94075938+czegledi-david@users.noreply.github.com>
2026-04-20 10:07:23 +0200

    Forms app

---
+++ gyakorlas_vzs2/gyakorlas_vzs2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace gyakorlas_vzs2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(75, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(626, 303);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(75, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Utolsó eladott pékáru";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(75, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 31);
+            this.label1.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(276, 400);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(575, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 53);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Szűrés dátum szerint";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ gyakorlas_vzs2/gyakorlas_vzs2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace gyakorlas_vzs2
+{
+    public partial class Form1 : Form
+    {
+        public class Tranzakcio
+        {
+            public int Nap { get; private set; }
+            public string Idopont { get; private set; }
+            public string Termek { get; private set; }
+            public int DolgozoId { get; private set; }
+            public int Mennyiseg { get; private set; }
+            public string Muvelet { get; private set; } // S vagy E
+            
+            public Tranzakcio(int nap, string idopont, string termek, int dolgozoid, int mennyiseg, char m)
+            {
+                Nap = nap;
+                Idopont = idopont;
+                Termek = termek;
+                DolgozoId = dolgozoid;
+                Mennyiseg = mennyiseg;
+                Muvelet = (m == 'S') ? "Sütés" : "Eladás";
+            }
+
+
+        }
+
+        private List<Tranzakcio> mozgasok = new List<Tranzakcio>();
+
+        private void AdatokBeolvasasa(string fajlNev)
+        {
+            try
+            {
+                string[] sorok = File.ReadAllLines(fajlNev);
+                foreach (string s in sorok) 
+                {
+                    if (string.IsNullOrWhiteSpace(s)) continue;
+
+                    string[] adatok = s.Split(' ');
+                    Tranzakcio t = new Tranzakcio(
+                        int.Parse(adatok[0]),
+                        adatok[1],
+                        adatok[2],
+                        int.Parse(adatok[3]),
+                        int.Parse(adatok[4]),
+                        char.Parse(adatok[5])
+                    );
+                    mozgasok.Add(t);
+                }
+                //Betölti az összes adatot a táblázatba induláskor
+                dataGridView1.DataSource = mozgasok;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba a fájl beolvasásakor: " + ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            AdatokBeolvasasa("pekseg.txt");
+        }
+
+        //Utolsó eladott gomb
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = mozgasok.Count -1; i >= 0; i--)
+            {
+                if (mozgasok[i].Muvelet == "Eladás")
+                {
+                    label1.Text = "Utolsó eladott pékáru: " + mozgasok[i].Termek;
+                    return; //Megtaláltuk, befejezzük a futást
+                }
+            }
+            label1.Text = "Nem volt eladás.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Csak a nap kell
+            int valasztottnap = dateTimePicker1.Value.Day;
+
+            List<Tranzakcio> szurtlista = new List<Tranzakcio>();
+
+            foreach(Tranzakcio t in mozgasok)
+            {
+                if (t.Nap == valasztottnap)
+                {
+                    szurtlista.Add(t);
+                } 
+            }
+            
+            //Ha nincs adat akkor kiürítjük a táblázatot
+            if(szurtlista.Count == 0)
+            {
+                dataGridView1.DataSource = null;
+                MessageBox.Show("Ezen a napon nem volt forgalom.", "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                //Kicseréljük a szűrt listára
+                dataGridView1.DataSource = szurtlista;
+            }
+
+
+
+        }
+    }
+}
